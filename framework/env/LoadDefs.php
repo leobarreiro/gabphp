6f0530c8a8196fb178f2947ff3fd98ae78de2784
Leopoldo Barreiro <leo@cielnews.com>
2008-08-23 12:34:41 +0000

    Inclusão Inicial

---
+++ framework/env/LoadDefs.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* LoadDefs.php
+* Carrega Definições para a página principal
+* 25/01/2008
+* */
+
+// variaveis de ambiente e variaveis especiais
+
+error_reporting(GBA_ERRORS);
+
+// includes básicas
+
+include_once ( GBA_PATH_CLA_EST .'Sistema.class.php' );
+include_once ( GBA_PATH_CLA_EST .'Sessao.class.php' );
+include_once ( GBA_PATH_CLA_EST .'Cookie.class.php' );
+include_once ( GBA_PATH_CLA_EST .'FusoHorario.class.php' );
+include_once ( GBA_PATH_CLA_BDA .'RecordSet.class.php' );
+
+// Verificar aqui a validade do sistema !!
+
+?>
---
+++ framework/env/env.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ 	* Framework GBAPHP
+    * @license : GNU General License v.1
+    * @link http://www.cielnews.com/gba
+    * 
+    * Página de Definição de Constantes do Sistema
+    * Data de Criação: 14/11/2006
+    * @author Leopoldo Braga Barreiro
+    *     
+    * @package GBAPHP
+    * @subpackage
+    *     
+    * $Id: $
+    *     
+    * Casos de uso : 
+*/
+
+define("GBA_ERRORS", 12); // Para depuracao
+
+error_reporting(GBA_ERRORS);
+
+define("GBA_URL_BASE", 'http://localhost/sistemas/');
+define("GBA_PATH_BASE", '/home/sistemas/');
+
+define("GBA_URL_SISTEMA", GBA_URL_BASE . 'gbaphp/');
+define("GBA_PATH_SISTEMA", GBA_PATH_BASE . 'gbaphp/');
+
+define("GBA_PATH_FWK", GBA_PATH_SISTEMA . 'Framework/');
+
+define("GBA_PATH_ENV", GBA_PATH_FWK . 'Env/');
+define("GBA_PATH_CLA", GBA_PATH_FWK . 'Classes/');
+define("GBA_PATH_CLA_EST", GBA_PATH_FWK . 'Classes/Estatica/');
+define("GBA_PATH_CLA_MAP", GBA_PATH_FWK . 'Classes/Mapeamento/');
+define("GBA_PATH_CLA_BDA", GBA_PATH_FWK . 'Classes/BancoDados/');
+define("GBA_PATH_CLA_CMP", GBA_PATH_FWK . 'Classes/Componentes/');
+define("GBA_PATH_CLA_INT", GBA_PATH_FWK . 'Classes/Componentes/Interface/');
+
+// 27/01/2007
+
+// Fpdf - Biblioteca para Gerar PDF
+
+define("FPDF_FONTPATH", GBA_PATH_FWK . 'Fpdf/font/');
+
+// Prefixo comum para todas as Constantes (SWB)
+
+// Familia de Constantes para Banco de Dados (BD)
+
+define("GBA_BD_HOST", 'localhost');
+define("GBA_BD_USR", 'gbaphp');  // Usuario Mestre para Conexao
+define("GBA_BD_PW", '123456');
+define("GBA_BD_NAME", 'gbaphp');  // Banco de Dados do Sistema
+
+// FTP
+
+define("GBA_FTP_HOST", 'localhost');
+define("GBA_FTP_USR", 'leo');  // Usuario Mestre para Conexao
+define("GBA_FTP_PW", 'fabi');
+define("GBA_FTP_PASSIVE", 'true');
+define("GBA_FTP_PATH", '/home/sistemas/gbaphp/');
+define("GBA_FTP_PATH_FOTOS", 'img/');
+
+// Tabelas do Banco
+
+define("GBA_BD_TEMPR", 'gba_empresa');
+define("GBA_BD_TUSUA", 'gba_usuario');
+define("GBA_BD_TSESS", 'gba_sessao');
+define("GBA_BD_TLOGS", 'gba_log');
+define("GBA_BD_TMODU", 'gba_modulo');
+define("GBA_BD_TFUNC", 'gba_funcionalidade');
+define("GBA_BD_TACAO", 'gba_acao');
+define("GBA_BD_TUSAC", 'gba_usuario_acao');
+define("GBA_BD_TEMMO", 'gba_empresa_modulo');
+
+// Modulos do Sistema
+// Valor 1=ativado 0=desativado
+
+define("GBA_MODULO_ADMINISTRACAO", 1);
+define("FNC_MODULO_COBRANCA", 1);
+define("FNC_MODULO_CLIENTES", 1);
+define('FNC_MODULO_SERVICOS', 1);
+
+// Imagens
+define("GBA_IMG_MAXWIDTH", 200);
+define("GBA_IMG_MAXHEIGHT", 200);
+define("GBA_IMG_MAXWEIGHT", 20000);
+
+// Thumbnails
+define("GBA_THUMB_MAXWIDTH", 80);
+define("GBA_THUMB_MAXHEIGHT", 80);
+define("GBA_THUMB_MAXWEIGHT", 2084);
+
+// Diretorios Imagens
+define("GBA_IMG_DIR_BASE", GBA_PATH_BASE . 'imagens/');
+define("GBA_IMG_DIR_THUMBS", 'thumbs/'); // caminho relativo ao diretorio principal de imagens
+define("GBA_IMG_FOTO_BORDA_EXT", 'black'); // 17/01/2007 - Cor da Borda Externa para Fotos
+define("GBA_IMG_FOTO_BORDA_INT", 'white'); // 17/01/2007 - Cor da Borda Interna para Fotos
+define("GBA_IMG_ETQ_FOTO", GBA_PATH_SISTEMA . 'Images/etq.gif');
+define("GBA_IMG_URL_BASE", GBA_URL_BASE . 'imagens_atusistemas/');
+
+// Titulo de Paginas do Sistema
+
+define("GBA_TITULO_PAGINA", 'GBAphp 0.1 beta');
+
+// Familia de Constantes para Sessao (SE)
+// Inatividade da Sessao
+define("GBA_SE_INATIV", 60*60*10);
+
+// Nome da Cookie
+define("GBA_COOKIE_NAME", 'GbaPhp01'); 
+
+// Validade do Sistema
+define("GBA_VALIDADE_SISTEMA", '0000-00-00');
+
+// Regionalizacao
+define("GBA_FUSO_HORARIO", '0');
+
+define('GBA_VERSION', 'GBAphp 0.1 beta');
+?>
