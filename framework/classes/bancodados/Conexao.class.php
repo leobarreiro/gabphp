b941192df4a6ea36554297ee045b2540cc7b69a6
Leopoldo Barreiro <leo@cielnews.com>
2009-06-27 11:45:24 +0000

    Corrigida identação do código-fonte. Incluído cabeçalho padrão do arquivo.

--- framework/classes/bancodados/Conexao.class.php
+++ framework/classes/bancodados/Conexao.class.php
@@ -1,100 +1,192 @@
 <?php
+/**
+ 	* Framework GBAPHP
+    * @license : GNU Lesser General Public License v.3
+    * @link http://www.cielnews.com/gba
+    * 
+    * Classe de Conexao com Base de Dados
+    * Data de Criacao: 05/01/2008
+    *     
+    * @package GBAPHP
+    * @subpackage
+    *     
+    * $Id: $
+    * $Author: $
+    * 
+    * Casos de uso : 
+*/
 
 class Conexao {
 
-var $stHost;
-var $stUsuario;
-var $stSenha;
-var $stBanco;
-var $idConexao;
-var $stMsg;
-var $inErro;
-var $roConsulta;
-
-function setHost($stHost) { $this->stHost = (string) $stHost; }
-function setUsuario($stUsuario) { $this->stUsuario = (string) $stUsuario; }
-function setSenha($stSenha) { $this->stSenha = (string) $stSenha; }
-function setBanco($stBanco) { $this->stBanco = (string) $stBanco; }
-function setMsg($stMsg) { $this->stMsg = (string) $stMsg; }
-function setErro($integer) { $this->inErro = (integer) $integer; }
-function setConexao($idConexao) { $this->idConexao = $idConexao; } /* Permite definir uma conexao ja existente */
-
-function getHost() { return $this->stHost; }
-function getUsuario() { return $this->stUsuario; }
-function getSenha() { return $this->stSenha; }
-function getBanco() { return $this->stBanco; }
-function getMsg() { return $this->stMsg; }
-function getErro() { return $this->inErro; }
-function getConexao() {return $this->idConexao; }
-
-function Conexao($stHost=null, $stUsuario=null, $stSenha=null, $stBanco=null) {
+	public $stHost;
+	public $stUsuario;
+	public $stSenha;
+	public $stBanco;
+	public $idConexao;
+	public $stMsg;
+	public $inErro;
+	public $roConsulta;
 	
-	if (strlen($stHost)) { $this->setHost($stHost); }
-	if (strlen($stUsuario)) { $this->setUsuario($stUsuario); }
-	if (strlen($stSenha)) { $this->setSenha($stSenha); }
-	if (strlen($stBanco)) { $this->setBanco($stBanco); }
-	
-	if ($this->getHost() && $this->getUsuario() && $this->getSenha()) {
-		$this->conecta();
+	public function setHost($stHost)
+	{
+		$this->stHost = (string) $stHost;
 	}
 	
-	if ($this->getBanco()) {
-		$this->selecionaBanco();
-	}	
+	public function setUsuario($stUsuario)
+	{
+		$this->stUsuario = (string) $stUsuario;
+	}
 	
-	if (is_resource($this->idConexao)) {		
-		if(mysql_error($this->getConexao())) {
-			$this->setMsg(mysql_error($this->getConexao()));
-			$this->setErro(2);
+	public function setSenha($stSenha)
+	{
+		$this->stSenha = (string) $stSenha;
+	}
+	
+	public function setBanco($stBanco)
+	{
+		$this->stBanco = (string) $stBanco;
+	}
+	
+	public function setMsg($stMsg)
+	{
+		$this->stMsg = (string) $stMsg;
+	}
+	
+	public function setErro($integer)
+	{
+		$this->inErro = (integer) $integer;
+	}
+	
+	/* Permite definir uma conexao ja existente */
+	public function setConexao($idConexao)
+	{
+		$this->idConexao = $idConexao;
+	}
+	
+	public function getHost()
+	{
+		return $this->stHost;
+	}
+	
+	public function getUsuario()
+	{
+		return $this->stUsuario;
+	}
+	
+	public function getSenha()
+	{
+		return $this->stSenha;
+	}
+	
+	public function getBanco()
+	{
+		return $this->stBanco;
+	}
+	
+	public function getMsg()
+	{
+		return $this->stMsg;
+	}
+	
+	public function getErro()
+	{
+		return $this->inErro;
+	}
+	
+	public function getConexao()
+	{
+		return $this->idConexao;
+	}
+	
+	public function Conexao($stHost=null, $stUsuario=null, $stSenha=null, $stBanco=null)
+	{	
+		if (strlen($stHost))
+		{
+			$this->setHost($stHost);
 		}
-		else {
-			$this->setMsg('Conectado corretamente.');
-			$this->setErro(0);
-		}		
-	}
-	else {
-		$this->setConexao(null);
-		$this->setMsg('N�o ocorreu a Conex�o');
-		$this->setErro(1);		
-	}
-
-}
-
-function conecta() {
-	$this->idConexao = mysql_connect($this->getHost(), $this->getUsuario(), $this->getSenha());
-}
-
-function selecionaBanco() {
-	mysql_select_db($this->getBanco());
-}
-
-/**
- * @name 		executaSQL
- * @desc 		Executa uma expressao SQL no banco
- * @param 		String stSQL
- * @return 		Resource 
-* */
-
-function executaSQL($sql) {
-	if (is_resource($this->idConexao)) {		
-		$this->roConsulta = mysql_query($sql, $this->idConexao);		
-		if (mysql_error($this->getConexao())) {
-			$this->setMsg( mysql_error($this->getConexao()) );
-			$this->setErro( mysql_errno($this->getConexao()) );
-			return false;
-		} else {
-			$this->setMsg('Sem erro');
-			$this->setErro(0);
-			return $this->roConsulta;
-		}		
-	} else {		
-		$this->setMsg('Sem conexao estabelecida');
-		$this->setErro(1);
-		return false;
+		if (strlen($stUsuario))
+		{
+			$this->setUsuario($stUsuario);
+		}
+		if (strlen($stSenha))
+		{
+			$this->setSenha($stSenha);
+		}
+		if (strlen($stBanco))
+		{
+			$this->setBanco($stBanco);
+		}
+		if ($this->getHost() && $this->getUsuario() && $this->getSenha())
+		{
+			$this->conecta();
+		}
+		if ($this->getBanco())
+		{
+			$this->selecionaBanco();
+		}	
+		if (is_resource($this->idConexao))
+		{
+			if (mysql_error($this->getConexao()))
+			{
+				$this->setMsg(mysql_error($this->getConexao()));
+				$this->setErro(2);
+			}
+			else
+			{
+				$this->setMsg('Conectado corretamente.');
+				$this->setErro(0);
+			}		
+		}
+		else
+		{
+			$this->setConexao(null);
+			$this->setMsg('Nao ocorreu a Conexao');
+			$this->setErro(1);
+		}
 	}
 	
-}
-
+	public function conecta()
+	{
+		$this->idConexao = mysql_connect($this->getHost(), $this->getUsuario(), $this->getSenha());
+		return $this->idConexao;
+	}
+	
+	public function selecionaBanco()
+	{
+		mysql_select_db($this->getBanco());
+	}
+	
+	/**
+	 * @name 		executaSQL
+	 * @desc 		Executa uma expressao SQL no banco
+	 * @param 		String stSQL
+	 * @return 		Resource 
+	* */
+	public function executaSQL($sql)
+	{
+		if (is_resource($this->idConexao))
+		{
+			$this->roConsulta = mysql_query($sql, $this->idConexao);
+			if (mysql_error($this->getConexao()))
+			{
+				$this->setMsg( mysql_error($this->getConexao()) );
+				$this->setErro( mysql_errno($this->getConexao()) );
+				return false;
+			}
+			else
+			{
+				$this->setMsg('Sem erro');
+				$this->setErro(0);
+				return $this->roConsulta;
+			}		
+		}
+		else
+		{
+			$this->setMsg('Sem conexao estabelecida');
+			$this->setErro(1);
+			return false;
+		}
+	}
 
 }
 ?>
