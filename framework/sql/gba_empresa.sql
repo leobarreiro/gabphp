8fd1c0a69ce1a6db94e6e9911ea230f0d7e1ffff
Leopoldo Barreiro <leo@cielnews.com>
2009-07-21 02:23:32 +0000

    Renomeado diretório base do framework.

--- framework/sql/gba_empresa.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `gba_empresa` (
-  `codempresa` int(2) NOT NULL auto_increment,
-  `nomeempresa` char(20) NOT NULL,
-  `prefixo` char(3) NOT NULL,
-  `descricao` char(60) default NULL,
-  `ativa` int(1) NOT NULL default '1',
-  PRIMARY KEY  (`codempresa`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
