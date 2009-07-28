e5534a1b2004923e90ee96a68f057b22b9de2a57
Leopoldo Barreiro <leo@cielnews.com>
2009-07-28 22:59:08 +0000

    Remoção de diretório obsoleto.

--- gabphp/trash/FazerLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 14/11/2006
-// Fazer Login leva o usu�rio at� a p�gina de login
-function FazerLogin($oplogin=1)
-{
-	header("Cache-control: private");
-	header("Location: " . ATU_URL_SISTEMA . "login.php?m=$oplogin");
-}
-?>
