d43e09dbe748e2a75dafaefd4a9802b8bcddb165
Leopoldo Barreiro <leo@cielnews.com>
2009-11-18 00:02:07 +0000

    Corrigido cabeçalho do fonte. Ajustada identação. Criado um alias para o método de adicionar componentes.

--- gabphp/classes/componentes/interface/IComponenteBase.class.php
+++ gabphp/classes/componentes/interface/IComponenteBase.class.php
@@ -1,106 +1,198 @@
 <?php
-/*
-* IComponenteBase.class.php
-* 28/12/2007
+/**
+ 	* Framework GabPHP
+    * @license : GNU Lesser General Public License v.3
+    * @link http://www.leopoldobarreiro.com/gabphp
+    * 
+    * Classe de Componente Basico para Interface HTML
+    * Data de Criacao: 28/12/2007
+    * @author Leopoldo Braga Barreiro
+    *     
+    * @package GABPHP
+    * @subpackage
+    *     
+    * $Id: $
+    *     
+    * Casos de uso : 
 */
 
 include_once( GBA_PATH_CLA . 'Object.class.php' );
 include_once (GBA_PATH_CLA_INT . 'IEvento.class.php');
 
 class IComponenteBase extends Object {
-
-var $stTag;
-var $stNome;
-var $stId;
-var $stType;
-var $obEvento;
-var $arComponente; // array de Objetos contidos
-var $stCss;
-var $stStyle;
-var $stTitle;
-var $stHtml;
-
-
-function IComponenteBase() {
-	parent::__construct();
-	$this->stCss = '';
-	$this->stStyle = '';
-	$this->stHtml = '';
-	$this->obEvento = new IEvento;
-	$this->arComponente = array();
-}
-
-function setTag($string) { $this->stTag = $string; }
-function setNome($valor) { $this->stNome = $valor; }
-function setId($valor) { $this->stId = $valor; }
-function setNomeId($string) { $this->stNome = $string; $this->stId = $string; }
-function setType($valor) { $this->stType = $valor; }
-function setCss($valor) { $this->stCss = $valor; }
-function setStyle($valor) { $this->stStyle = $valor; }
-function setTitle($stTitle) { $this->stTitle = $stTitle; }
-
-function getTag() { return $this->stTag; }
-function getNome() { return $this->stNome; }
-function getId() { return $this->stId; }
-function getType() { return $this->stType; }
-function getCss() { return $this->stCss; }
-function getStyle() { return $this->stStyle; }
-function getTitle() { return $this->stTitle; }
-
-function addComponente($mixedComponente) {
-	if (is_object($mixedComponente)) {
-		$this->arComponente[] = $mixedComponente;	
-	} elseif (is_array($mixedComponente)) {
-		foreach ($mixedComponente as $obComponente) {
-			$this->arComponente[] = $obComponente;
+	
+	var $stTag;
+	var $stNome;
+	var $stId;
+	var $stType;
+	var $obEvento;
+	var $arComponente; // array de Objetos contidos
+	var $stCss;
+	var $stStyle;
+	var $stTitle;
+	var $stHtml;
+	
+	
+	public function IComponenteBase()
+	{
+		parent::__construct();
+		$this->stCss = '';
+		$this->stStyle = '';
+		$this->stHtml = '';
+		$this->obEvento = new IEvento;
+		$this->arComponente = array();
+	}
+	
+	public function setTag($string)
+	{
+		$this->stTag = $string;
+	}
+	
+	public function setNome($valor)
+	{
+		$this->stNome = $valor;
+	}
+	
+	public function setId($valor)
+	{
+		$this->stId = $valor;
+	}
+	
+	public function setNomeId($string)
+	{
+		$this->stNome = $string; $this->stId = $string;
+	}
+	
+	public function setType($valor)
+	{
+		$this->stType = $valor;
+	}
+	
+	public function setCss($valor)
+	{
+		$this->stCss = $valor;
+	}
+	
+	public function setStyle($valor)
+	{
+		$this->stStyle = $valor;
+	}
+	
+	public function setTitle($stTitle)
+	{
+		$this->stTitle = $stTitle;
+	}
+	
+	public function getTag()
+	{
+		return $this->stTag;
+	}
+	
+	public function getNome()
+	{
+		return $this->stNome;
+	}
+	
+	public function getId()
+	{
+		return $this->stId;
+	}
+	
+	public function getType()
+	{
+		return $this->stType;
+	}
+	
+	public function getCss()
+	{
+		return $this->stCss;
+	}
+	
+	public function getStyle()
+	{
+		return $this->stStyle;
+	}
+	
+	public function getTitle()
+	{
+		return $this->stTitle;
+	}
+	
+	public function addComponente($mixedComponente)
+	{
+		if (is_object($mixedComponente))
+		{
+			$this->arComponente[] = $mixedComponente;	
 		}
-	}	
-}
-
-// Metodo Base para Implementacao Especialista
-function montaHtml() {
-	$stHtml = "\n" . '<' . $this->getTag();
-	if (strlen($this->getNome())) {
-		$stHtml .= ' name="' . $this->getNome() . '"';
+		elseif (is_array($mixedComponente))
+		{
+			foreach ($mixedComponente as $obComponente)
+			{
+				$this->arComponente[] = $obComponente;
+			}
+		}
 	}
-	if (strlen($this->getId())) {
-		$stHtml .= ' id="' . $this->getId() . '"';
-	}
-	if (strlen($this->getCss())) {
-		$stHtml .= ' class="' . $this->getCss() . '"';
-	}
-	if (strlen($this->getStyle())) {
-		$stHtml .= ' style="' . $this->getStyle() . '"';
-	}
-	if (strlen($this->getTitle())) {
-		$stHtml .= ' title="' . $this->getTitle() . '"';
-	}	
 	
-	$this->obEvento->montaHtml();
-	$stHtml .= $this->obEvento->getHtml();
-	
-	$stHtml .= '>';
-	
-	foreach ($this->arComponente as $obComponente) {
-		$obComponente->montaHtml();
-		$stHtml .= $obComponente->getHtml();
+	public function add($mixedComponente)
+	{
+		$this->addComponente($mixedComponente);
 	}
+	
+	// Metodo Base para Implementacao Especialista
+	public function montaHtml()
+	{
+		$stHtml = "\n" . '<' . $this->getTag();
+		if (strlen($this->getNome()))
+		{
+			$stHtml .= ' name="' . $this->getNome() . '"';
+		}
+		if (strlen($this->getId()))
+		{
+			$stHtml .= ' id="' . $this->getId() . '"';
+		}
+		if (strlen($this->getCss()))
+		{
+			$stHtml .= ' class="' . $this->getCss() . '"';
+		}
+		if (strlen($this->getStyle()))
+		{
+			$stHtml .= ' style="' . $this->getStyle() . '"';
+		}
+		if (strlen($this->getTitle()))
+		{
+			$stHtml .= ' title="' . $this->getTitle() . '"';
+		}	
 		
-	$stHtml .= '</' . $this->getTag() . '>';
-	$this->stHtml = $stHtml; 
-}
-
-// Metodo Padrao para imprimir HTML
-function getHtml() { return $this->stHtml; }
-
-function show() {
-	$this->montaHtml(); 
-	echo $this->stHtml;
-}
-
-function renderizar() {
-	$this->show();
-}
-
+		$this->obEvento->montaHtml();
+		$stHtml .= $this->obEvento->getHtml();
+		
+		$stHtml .= '>';
+		
+		foreach ($this->arComponente as $obComponente)
+		{
+			$obComponente->montaHtml();
+			$stHtml .= $obComponente->getHtml();
+		}
+			
+		$stHtml .= '</' . $this->getTag() . '>';
+		$this->stHtml = $stHtml; 
+	}
+	
+	// Metodo Padrao para imprimir HTML
+	public function getHtml()
+	{
+		return $this->stHtml;
+	}
+	
+	public function show()
+	{
+		$this->montaHtml(); 
+		echo $this->stHtml;
+	}
+	
+	function renderizar()
+	{
+		$this->show();
+	}
 }
 ?>
